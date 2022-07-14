57f180d7651ba4690f96274cb9b076a98cecbf07
Aayushi-Tyagi <aayushitya@gmail.com>
2022-07-14 11:31:18 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,254 @@
+let songs = [
+{
+    
+    name: 'Backbone',
+    path: 'musics/Backbone.mp3',
+    artist: 'Hardy Sandhu',
+    cover: 'images/Backbone.jpg'
+},
+{
+    
+    name: 'Badaami Rang',
+    path: 'musics/Badaami Rang.mp3',
+    artist: 'Nikk',
+    cover: 'images/Badaami Rang.jpg'
+},
+{
+     name: 'Bhala',
+     path: 'musics/Bhala.mp3',
+     artist: 'Hansraj Raghuwanshi',
+     cover: 'images/Bhala.jpg'
+},
+{
+    
+    name: 'Bijlee',
+    path: 'musics/Bijlee Bijlee.mp3',
+    artist: 'Hardy Sandhu',
+    cover: 'images/Bijlee.jpg'
+},
+{
+    name: 'Butterfly',
+    path: 'musics/Butterfly.mp3',
+    artist: 'Jass Manak',
+    cover: 'images/Butterfly.jpg'
+},
+{
+    
+    name: 'Chaar Chudiyaan',
+    path: 'musics/Chaar Chudiyaan.mp3',
+    artist: 'Nikk',
+    cover: 'images/Chaar Chudiyaan.jpg'
+},
+{
+    name: 'Damru Bajaye',
+    path: 'musics/Damru Bajaye.mp3',
+    artist: 'Hansraj Raghuwanshi',
+    cover: 'images/Damru Bajaye.jpg'
+},
+{
+    name: 'Gurnazar Medley',
+    path: 'musics/Gurnazar Medley.mp3',
+    artist: 'Gurnazar Chattha',
+    cover: 'images/Gurnazar Medley.jpg'
+},
+{
+    name: 'Hasdi Reya Kar',
+    path: 'musics/Hasdi Reya Kar.mp3',
+    artist: 'Gurnazar Chattha',
+    cover: 'images/Hasdi Reya Kar.jpg'
+},
+{
+    name: 'Hauli Hauli',
+    path: 'musics/Hauli Hauli.mp3',
+    artist: 'Gurnazar Chattha',
+    cover: 'images/Hauli Hauli.jpg'
+},
+{
+    name: 'Horn Blow',
+    path: 'musics/Horn Blow.mp3',
+    artist: 'Hardy Sandhu',
+    cover: 'images/Horn Blow.jpg'
+},
+{
+    name: 'I Promise',
+    path: 'musics/I Promise.mp3',
+    artist: 'Gurnazar Chattha',
+    cover: 'images/I Promise.jpg'
+},
+{
+    name: 'Jee Karr Daa',
+    path: 'musics/Jee Karr Daa.mp3',
+    artist: 'Hardy Sandhu',
+    cover: 'images/Jee karda daa.jpg'
+},
+{
+    name: 'Kar Promise',
+    path: 'musics/Kar Promise.mp3',
+    artist: 'Gurnazar Chattha',
+    cover: 'images/Kar Promise.jpg'
+},
+{
+    name: 'Khair Mangdi',
+    path: 'musics/Khair Mangdi.mp3',
+    artist: 'Hardy Sandhu',
+    cover: 'images/Khair Mangdi.jpg'
+},
+{
+    name: 'Koi Vi Nahi',
+    path: 'musics/Koi Vi Nahi.mp3',
+    artist: 'Gurnazar Chattha',
+    cover: 'images/Koi Vi Nahi.jpg'
+},
+{
+    name: 'Kudi Kudi',
+    path: 'musics/Kudi Kudi.mp3',
+    artist: 'Gurnazar',
+    cover: 'images/Kudi Kudi.jpg'
+},
+{
+    name: 'Laagi Lagan Shankara',
+    path: 'musics/Laagi Lagan Shankara.mp3',
+    artist: 'Hansraj Raghwanshi',
+    cover: 'images/Mere Shankara.jpg'
+},
+{
+    name: 'Lehanga',
+    path: 'musics/Lehanga.mp3',
+    artist: 'Jass Manak',
+    cover: 'images/Lehenga.jpg'
+},
+{
+    name: 'Main Shiv Ka Shiv Mere',
+    path:'musics/Main Shiv Ka Shiv Mere.mp3',
+    artist: 'Hansraj Raghuwanshi',
+    cover: 'images/Main Shiv Ka Shiv Mere.jpg'
+},
+{
+    name: 'Naa ji Naa',
+    path: 'musics/Naa Ji Naa.mp3',
+    artist: 'Hardy Sandhu',
+    cover: 'images/Naa ji Naa.jpg'
+},
+{
+    name: 'Naah',
+    path: 'musics/Naah.mp3',
+    artist: 'Hardy Sandhu',
+    cover: 'images/Naah.jpg'
+},
+{
+    name: 'Nakhre Tere',
+    path: 'musics/Nakhre Tere.mp3',
+    artist: 'Nikk',
+    cover: 'images/Nakhre Tere.jpg'
+},
+{
+    name: 'No Competition',
+    path: 'musics/No Competition.mp3',
+    artist: 'Jass Manak',
+    cover: 'images/No competition.jpg'
+},
+{
+    name: 'Pinjraa',
+    path: 'musics/Pinjraa.mp3',
+    artist: 'Gurnazar Chattha',
+    cover: 'images/Pinjraa.jpg'
+},
+{
+    name: 'Shiv sama rahe',
+    path: 'musics/Shiv sama rahe.mp3',
+    artist: 'Hansraj Raghwanshi',
+    cover: 'images/Shiv sama rahe.jpg'
+},
+{
+    name: 'Tabaah',
+    path: 'musics/Tabaah.mp3',
+    artist: 'Gurnazar Chattha',
+    cover: 'images/Tabaah.jpg'
+},
+{
+    name: 'Titliaan',
+    path: 'musics/Titliaan.mp3',
+    artist: 'Hardy Sandhu',
+    cover: 'images/Titliaan.jpg'
+},
+{
+    name: 'song 2',
+    path: 'musics/song 2.mp3',
+    artist: 'artist 2',
+    cover: 'images/cover 2.png'
+},
+{
+    name: 'song 3',
+    path: 'musics/song 3.mp3',
+    artist: 'artist 3',
+    cover: 'images/cover 3.png'  
+},
+{
+    name: 'song 4',
+    path: 'musics/song 4.mp3',
+    artist: 'artist 4',
+    cover: 'images/cover 4.png'
+},
+{
+    name: 'song 5',
+    path: 'musics/song 5.mp3',
+    artist: 'artist 5',
+    cover: 'images/cover 5.png'
+},
+{
+    name: 'song 6',
+    path: 'musics/song 6.mp3',
+    artist: 'artist 6',
+    cover: 'images/cover 6.png'
+},
+{
+    name: 'song 7',
+    path: 'musics/song 7.mp3',
+    artist: 'artist 7',
+    cover: 'images/cover 7.png'
+},
+{
+    name: 'song 8',
+    path: 'musics/song 8.mp3',
+    artist: 'artist 8',
+    cover: 'images/cover 8.png'
+},
+
+{
+    name: 'Wah Ji Waah',
+    path: 'musics/Wah Ji Waah.mp3',
+    artist: 'Gurnazar Chattha',
+    cover: 'images/Wah Ji Waah.jpg'
+},
+{
+    name: 'Yaara Tere Warga',
+    path: 'musics/Yaara Tere Warga.mp3',
+    artist: 'Jass Manak',
+    cover: 'images/yaara tera warga.jpeg'
+},
+{
+    name: 'Yes Or No',
+    path: 'musics/Yes Or No.mp3',
+    artist: 'Jass Manak',
+    cover: 'images/yes or no.jpg'
+},
+{
+    name: 'Yaari',
+    path: 'musics/Yaari.mp3',
+    artist: 'Nikk',
+    cover: 'images/Yaari.jpg'
+},
+{
+    name: 'Yaarr Ni Milyaa',
+    path: 'musics/Yaarr Ni Milyaa.mp3',
+    artist: 'Hardy Sandhu',
+    cover: 'images/Yaarr Ni Miliyaa.jpg'
+},
+{
+    name: 'Yaarr Superstaar',
+    path: 'musics/Yaarr Superstaar.mp3',
+    artist: 'Hardy Sandhu',
+    cover: 'images/Yaarr Superstaar.jpg'
+},
+   
+]
